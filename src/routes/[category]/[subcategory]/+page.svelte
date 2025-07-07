4d58a2dfa4601a73232be43a0afe269db33bb9b8
David Haz <davidhzdev@gmail.com>
2025-07-07 23:15:52 +0300

    remove boilerplate

---
+++ src/routes/[category]/[subcategory]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	
+	$: category = $page.params.category;
+	$: subcategory = $page.params.subcategory;
+</script>
+
+<div>
+	<h1>Category: {category}</h1>
+	<h2>Subcategory: {subcategory}</h2>
+</div>
